a719d3a3bc9ef154f96a8f804795346babe3cdd0
Leandre <lvdehee@gmail.com>
2018-12-14 13:50:15 +0100

    Init

---
+++ lib/m1nras_palindrome.rb
@@ -0,0 +1,6 @@
+require "m1nras_palindrome/version"
+
+module M1nrasPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/m1nras_palindrome/version.rb
@@ -0,0 +1,3 @@
+module M1nrasPalindrome
+  VERSION = "0.1.0"
+end
